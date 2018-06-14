2afb47b519ccf9dbb336159d374f78335c61bd20
hoshira <luoluosmile@gmail.com>
2018-06-14 12:19:00 +0200

    UI and fundemental code

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -19,7 +19,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var flipCount: Int = 0 {
+        didSet { // if flipCount changed, then excute the code below
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
 
-
+    
+    @IBOutlet var cardsCollection: [UIButton]!
+    
+    var emojiString : [String] = ["😬","😑","😞","🙃"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func tapCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber: Int = cardsCollection.index(of: sender){
+            print("Card Number is : \(cardNumber)")
+            flipCard(withEmoji: emojiString[cardNumber], onButton: sender)
+        }else{
+            print("The selected card is not in the card set.")
+        }
+    }
+    
+    //flip a card
+    //external name, internal name, type
+    func flipCard(withEmoji emoji: String, onButton button: UIButton){
+        print("flipCard withEmoji: \(emoji)")
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.01680417731, green: 0.1983509958, blue: 1, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
 }
 
